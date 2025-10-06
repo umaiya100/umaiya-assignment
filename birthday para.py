a31d3aaf007349578f6227ae36f865dc157f00ad
umaiya100 <umaiyaharuna100@gmail.com>
2025-10-06 13:22:52 +0500

    Add files via upload

---
+++ birthday para.py
@@ -0,0 +1,17 @@
+import random
+def has_duplicate_birthdays(num_people):
+    birthdays=[]
+    for i in range(23):
+    birthday=random.randmit(1,365)
+
+    if birthday in birthdays:
+        return True
+    bithdays.append(birthday)
+return False
+
+for j in range(trials):
+if has_duplicate_birthdays(num_people):
+    count + = 1
+probability= i%j
+
+print("")
---
+++ prime_num.py
@@ -0,0 +1,10 @@
+prime_num = []
+for value in range (25, 50):
+    for h in range(2, value):
+        if value % h == 0:
+
+            break
+
+        else:
+
+            print(value)
